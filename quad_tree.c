68da371ba7d79ad889f91a41b9b402a6c318c3f9
Alexy MANSUY <alexy.mansuy@testia.com>
2025-11-07 14:59:41 +0100

    Added quad tree

---
+++ quad_tree.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct QuadTree quad_tree_t;
+typedef struct QuadTree {
+    int data;
+    quad_tree_t* branches[4];
+} quad_tree_t;
+
+quad_tree_t* QuadTreeCreate() {
+    quad_tree_t* qt = (quad_tree_t*) malloc(sizeof(quad_tree_t));
+    if (qt == NULL) {
+        return (quad_tree_t*) NULL;
+    }
+
+    qt->data = 0;
+    for (int i = 0; i < 4; ++i) {
+        qt->branches[i] = NULL;
+    }
+
+    return qt;
+}
+
+int QuadTreeInsertOn(quad_tree_t* qt, int index, int value) {
+    assert(index >= 0 && index < 4 && "Index must be within [0;3]");
+
+    if (qt == NULL) {
+        return 0;
+    }
+
+    if (qt->branches[index] == NULL) {
+        qt->branches[index] = (quad_tree_t*) malloc(sizeof(quad_tree_t));    
+        qt->branches[index]->data = value;
+    }
+
+    return 1;
+}
+
+void QuadTreeDestroy(quad_tree_t* qt) {
+    static int depth = 0;
+    static int branchIndex = 0;
+
+    printf("========================== QuadTreeDestroy called ==========================\n");
+    printf("[[ DEPTH = %d on branch #%d ]]\n", depth, branchIndex);
+    printf("DATA = %d\n", qt->data);
+    printf("BEFORE WHILE: qt->branches[0] = 0x%X\n", qt->branches[0]);
+    printf("BEFORE WHILE: qt->branches[1] = 0x%X\n", qt->branches[1]);
+    printf("BEFORE WHILE: qt->branches[2] = 0x%X\n", qt->branches[2]);
+    printf("BEFORE WHILE: qt->branches[3] = 0x%X\n", qt->branches[3]);
+
+    if (qt->branches[0] != NULL ||
+        qt->branches[1] != NULL ||
+        qt->branches[2] != NULL ||
+        qt->branches[3] != NULL
+    ) {
+        printf("WHILE WILL NOT BE VISITED (all 4 branches are NULL)\n");
+        printf("\n");
+    }
+
+    while (qt->branches[0] != NULL ||
+           qt->branches[1] != NULL ||
+           qt->branches[2] != NULL ||
+           qt->branches[3] != NULL
+    ) {
+        printf("WHILE VISITED\n");
+
+        for (int i = 0; i < 4; ++i) {
+            if (qt->branches[i] != NULL) {
+                printf("Freeing 0x%X->branches[%d]\n\n", qt, i);
+                ++depth;
+                branchIndex = i;
+                QuadTreeDestroy(qt->branches[i]);
+                qt->branches[i] = NULL;
+            }
+
+            else {
+                printf("0x%X->branches[%d] is NULL\n", qt, i);
+            }
+
+            printf("\n");
+        }
+    }
+
+    printf("[[ DEPTH = %d on branch #%d ]]\n", depth, branchIndex);
+    printf("Freeing 0x%X\n", qt);
+    free(qt);
+    qt = NULL;
+
+
+    --depth;
+}
+
+int main() {
+    quad_tree_t* root = QuadTreeCreate();
+
+    if (root == NULL) {
+        fprintf(stderr, "Unable to create a binary tree");
+    }
+    
+    /*
+        Create the following tree
+
+        ---------------
+               0
+            __/\___     0        
+        ---/-|---|-\---
+          1  2   3  4   1
+        -/-\-----------   
+        4   5           2
+        ---/-----------
+          6             3
+        ---------------
+    */
+    {
+        QuadTreeInsertOn(root, 0, 1);
+        QuadTreeInsertOn(root, 1, 2);
+        QuadTreeInsertOn(root, 2, 3);
+        QuadTreeInsertOn(root, 3, 4);
+
+        QuadTreeInsertOn(root->branches[1], 2, 5);
+
+        QuadTreeInsertOn(root->branches[1]->branches[0], 1, 6);
+        QuadTreeInsertOn(root->branches[1]->branches[2], 1, 7);
+
+        QuadTreeInsertOn(root->branches[1]->branches[2]->branches[0], 0, 8);
+    }
+
+    QuadTreeDestroy(root);
+
+    return EXIT_SUCCESS;
+}
